d50092244e75ff9e8c64e38609b17481e3799bcf
Mathieu Bourdenx <mbourdenx@me.com>
2022-06-14 09:51:06 +0100

    change data

--- src/config.js
+++ src/config.js
@@ -2,9 +2,9 @@ function config() {
     var ini = [
         {   // 1.
             name: 'default',
-            img_width: 5905,
-            img_height: 5882,
-            img_depth: 15 * 6.0121,
+            img_width: 10978,
+            img_height: 9062,
+            img_depth: 20 * 3.8462 * 0.9,
             particle_size: 8000.0,
             // zThres: 1500.0,
             geneData: 'https://api.github.com/repos/acycliq/B2A3/contents/data/geneData?ref=master',
